760fc001322a1357cf4aa32358bc8a3425526335
MaryamElhamidi <unicolmaryame@gmail.com>
2023-11-17 20:43:13 -0500

    list created

--- Bank.py
+++ Bank.py
@@ -7,9 +7,12 @@ from Account import Account
 class Bank:
     def __init__(self, bankName):
         self.__bankName__ = bankName
-        self.accounts = [ChequingAccount("111", 5000.0, 1000.0),
-            ChequingAccount("222", 3000.0, 2000.0),
-            ChequingAccount("333", 7000.0, 1500.0),]
+        self.accounts = [ChequingAccount("111", "John Doe", 0.02, 5000.0, 1000.0),
+                         ChequingAccount("222", "Jane Doe", 0.03, 3000.0, 2000.0),
+                         ChequingAccount("333", "Bob Smith", 0.01, 7000.0, 1500.0),
+                         SavingsAccount("444", "Alice Johnson", 0.05, 8000.0, 5000.0),
+                         SavingsAccount("555", "Charlie Brown", 0.04, 6000.0, 3000.0),
+                         SavingsAccount("666", "Eva Green", 0.02, 9000.0, 4500.0)]
     
     def searchAccount():
         for account in accounts:  
