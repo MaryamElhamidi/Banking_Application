fe90af1e0223beccf870e7e12da770a0e795259b
MaryamElhamidi <unicolmaryame@gmail.com>
2023-11-20 19:38:32 -0500

    Added Functions to Bank

--- Bank.py
+++ Bank.py
@@ -5,12 +5,7 @@ import random
 class Bank():
     def __init__(self, bankName):
         self.__bankName__ = bankName
-        self.accounts = [ChequingAccount("111", "John Doe", 0.02, 5000.0, 1000.0),
-                         ChequingAccount("222", "Jane Doe", 0.03, 3000.0, 2000.0),
-                         ChequingAccount("333", "Bob Smith", 0.01, 7000.0, 1500.0),
-                         SavingsAccount("444", "Alice Johnson", 0.05, 8000.0, 5000.0),
-                         SavingsAccount("555", "Charlie Brown", 0.04, 6000.0, 3000.0),
-                         SavingsAccount("666", "Eva Green", 0.02, 9000.0, 4500.0)]
+        self.accounts = []
     
     def searchAccount(self, accountNumber):
         for account in self.accounts:  
@@ -19,8 +14,20 @@ class Bank():
                 print(accountNumber)
                 return account
         return None  # Account not found
+    def addAccount(self, account):
+        return self.accounts.append(account)
 
-    
+    def createAccountNumber(self):
+        return ''.join(str(random.randint(0, 9)) for _ in range(random.randint(8, 12)))
+
+    def createAccount(self):
+        self.addAccount(ChequingAccount(self.createAccountNumber(), 500, 1000))
+        self.addAccount(ChequingAccount(self.createAccountNumber(), 5000, 1500))
+        self.addAccount(ChequingAccount(self.createAccountNumber(), 1500, 5000))
+        self.addAccount(SavingsAccount((self.createAccountNumber(), 10000, 5000)))
+        self.addAccount(SavingsAccount(self.createAccountNumber(), 15000, 6000))
+        self.addAccount(SavingsAccount(self.createAccountNumber(), 20000, 4000))
+
     def openAccount(self, accountType, accountNumber = None, accountHolderName = None, rateOfInterest = None, currentBalance = None, minimumBalance = None, overdraftLimit = None):
         accountNumber = random.randint(111111, 999999)
         
